f78822da185698aefce19daddaa0fade470de26a
Oskar <oskaras34@gmail.com>
2019-12-26 20:01:41 +0200

    removed useless code and made elemenet editable after click, not everything finished

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const images = document.querySelectorAll(".gallery img").forEach(image => {
   image.addEventListener("click", () => {
-    
+ 
 // select image
 const enlargedImage = document.querySelector(".right-side");
 enlargedImage.innerHTML = ''
@@ -9,26 +9,56 @@ enlargedImage.innerHTML = ''
     let firstP = document.createElement('p');
     firstP.innerHTML = "Hello grab and drag me";
     let counter = 0;
-          // make paragraph visible
-          firstP.style.display = 'block';
-          // to drag element
-          firstP.style.position = 'absolute';
-          let newGrab = document.createElement('div');
-          newGrab.setAttribute('class', 'grab-' + counter);
-          newGrab.style.position = 'absolute';
-          newGrab.style.cursor = 'move';
-          newGrab.append(firstP)
+    // make paragraph visible
+    firstP.style.display = 'block';
+    // to drag element
+    firstP.style.position = 'absolute';
+    let newGrab = document.createElement('div');
+    newGrab.setAttribute('class', 'grab-' + counter);
+    newGrab.style.position = 'absolute';
+    newGrab.style.cursor = 'move';
+    newGrab.append(firstP)
+
+    let secondP = document.createElement('p');
+    secondP.innerHTML = "Hello grab and drag me";
+    // make paragraph visible
+    secondP.style.display = 'block';
+    // to drag element
+    secondP.style.position = 'absolute';
+    let grabTwo = document.createElement('div');
+    grabTwo.setAttribute('class', 'grab-1');
+    grabTwo.style.position = 'absolute';
+    grabTwo.style.cursor = 'move';
+    grabTwo.append(secondP)
 
     paragraphArray.push(newGrab);
+    paragraphArray.push(grabTwo)
 
+    let countMe = 0;
+    let clickCounter = '';
+    let clickedEl = ''
     paragraphArray.forEach((newGrabClass, counter) => {
       enlargedImage.append(newGrabClass);
       dragElement(document.querySelector('.grab-' + counter), counter);
+
+      // click on text
+      newGrabClass.addEventListener('click', () => {
+        // do not activate click, when dragging element
+        newGrabClass.addEventListener('click', () => {
+          paragraphArray.forEach(grabEl => {
+            grabEl.classList.remove('editable')
+          });
+
+          newGrabClass.classList.add('editable');
+          clickCounter = counter;
+          console.log(newGrabClass)
+          selectElemenetToEdit('.editable', clickCounter);
+        })
+      })
     });
     showImage(image, enlargedImage);
   });
 });
-
 // enlarge image
 function showImage(clickedImage, imageLocation) {
 
--- js/text-style.js
+++ js/text-style.js
@@ -1,27 +1,32 @@
+function selectElemenetToEdit (selectedEl, pId) {
 // select text color
 let selectColor = document.getElementById('color');
 selectColor.addEventListener('change', () => {
-    document.querySelector('.grab p').style.color = selectColor.value;
-});
-
+    document.querySelector(selectedEl + ' p').style.color = selectColor.value;
+    document.querySelector(selectedEl + ' p').style.border = "2px solid black";
+    console.log(document.querySelector(selectedEl))
+})
 // users inserted text
-const userText = document.getElementById('userText')
+const userText = document.getElementById('userText');
 userText
 .addEventListener('change', () => {
-    document.querySelector('.grab p').innerHTML = userText.value;
+    document.querySelector(selectedEl + ' p').innerHTML = userText.value;
 });
 
 // user text font-size
-const fontSize = document.getElementById('font-size')
+const fontSize = document.getElementById('font-size');
 fontSize
 .addEventListener('change', () => {
-    document.querySelector('.grab p').style.fontSize = fontSize.value
+    document.querySelector(selectedEl + ' p').style.fontSize = fontSize.value
     document.getElementById('selectedSize').innerHTML = fontSize.value + 'px'
 });
 
 // rotation for now only one paragraph
-const angle = document.getElementById('angle')
+const angle = document.getElementById('angle');
 angle.addEventListener('change', () => {
-  $("p").rotate({angle: parseInt(angle.value)});
+  $("p").eq(pId).rotate({angle: parseInt(angle.value)});
   document.getElementById('angle-value').innerHTML = angle.value
 });
+
+return true
+}
