2dbfbbc72f8fa8a2b14258cc07ca18694cdd75c1
Oskar <oskaras34@gmail.com>
2019-12-27 23:14:34 +0200

    cleaned code

--- js/text-style.js
+++ js/text-style.js
@@ -6,6 +6,7 @@ selectTextColor.addEventListener('change', () => {
     document.querySelector(selectedEl + ' p').style.color = selectTextColor.value;
 })
 
+// select text bg color
 let selectTextBgColor = document.getElementById('text-bg-color');
 selectTextBgColor.addEventListener('change', () => {
     document.querySelector(selectedEl + ' p').style.backgroundColor = selectTextBgColor.value;
@@ -32,6 +33,4 @@ angle.addEventListener('change', () => {
   $(selectedEl).rotate({angle: parseInt(angle.value)});
   document.getElementById('angle-value').innerHTML = angle.value
 });
-
-return true
 }
